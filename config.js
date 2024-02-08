9e06d83da86c1c206baaffbdb60d7324376b6051
Nicolas Pierrot <nicolas@archipels.io>
2024-02-08 11:15:50 +0100

    feat: project

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: "AKIAVBSLGJTGURMX7N5U", // TODO
+    secretAccessKey: "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", // TODO
+    endpoint: "https://esgi-cloud-exam-bucket-30849.s3.eu-west1.amazonaws.com", // TODO
+    bucket: "esgi-cloud-exam-bucket-30849", // TODO
+  },
+  suffix: {
+    small: "_small.jpg",
+    full: "_full.jpg",
+  },
+};
